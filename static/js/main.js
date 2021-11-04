e5ec7593b5df3766849d83eccf1ba8c140476ca4
Joshua inyang <joshuainyang255@gmail.com>
2021-11-04 05:55:48 -0700

    adding images of playing cards and soundes to the game

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#blackjack-hit-button').addEventListener('click', buttonHit);
+
+function buttonHit(){
+    alert('You just hit the blue buttton');
+}
+
+
+// console.log('NIce one')
